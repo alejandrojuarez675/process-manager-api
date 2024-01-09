ef55f318508c1d95181d28a21019f47baeb0f255
alejandro juarez <ext_alejjuar@mercadolibre.com>
2024-01-09 16:01:10 -0300

    first commit

---
+++ src/test/java/com/alejua/core/processmanagerapi/ProcessManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejua.core.processmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
